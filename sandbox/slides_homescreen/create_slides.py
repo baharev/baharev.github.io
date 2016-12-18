20d3366eede460b347508665382ee1d2f94d6970
baharev <ali.baharev@gmail.com>
2016-12-18 11:47:59 +0100

    Still leaks with jQuery's remove too

--- sandbox/slides_homescreen/create_slides.py
+++ sandbox/slides_homescreen/create_slides.py
@@ -90,10 +90,10 @@ PREAMBLE = \
     <nav>
       <div class="menubar">
           <div class="menuitem">
-            <span onclick="$.get('{prev}',function(data){{var d=document.open('text/html','replace');d.write(data);d.close();}});">&#x21e6;&nbsp;Előző</span>
+            <span onclick="$.get('{prev}',function(data){{$('html').remove(); var d=document.open('text/html','replace');d.write(data);d.close();}});">&#x21e6;&nbsp;Előző</span>
           </div>
          <div class="menuitem">
-            <span onclick="$.get('{next}',function(data){{var d=document.open('text/html','replace');d.write(data);d.close();}});">&#x21e8;&nbsp;Következő</span>
+            <span onclick="$.get('{next}',function(data){{$('html').remove(); var d=document.open('text/html','replace');d.write(data);d.close();}});">&#x21e8;&nbsp;Következő</span>
           </div>
       </div>
     </nav>
